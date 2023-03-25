6e1f8fb97b7bdd274fdea8af36be47e451c0919b
Dasun Dileka <dasundileka1@gmail.com>
2023-03-25 23:19:47 +0530

    Add project files.

---
+++ DataLayer/ContactsAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class ContactsAPIDbContext:DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options) 
+        { 
+        }
+        public DbSet<Product> Product{ get; set;}
+    
+    }
+}
---
+++ Loan-Provide/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using ApplicationLayer;
+using DataLayer;
+using DataLayer.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Loan_Provide.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductController : Controller
+    {
+
+        private ContactsAPIDbContext dbContext;
+        public ProductController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await dbContext.Product.ToListAsync());
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(ProductDto addProduct)
+        {
+            var productc = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Image = addProduct.Image,
+                Name = addProduct.Name,
+                Description = addProduct.Description,
+                Price = addProduct.Price,
+            };
+            await dbContext.Product.AddAsync(productc);
+            await dbContext.SaveChangesAsync();
+            return Ok(productc);
+        }
+
+    }
+}
